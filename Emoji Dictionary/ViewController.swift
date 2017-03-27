820d0447c0d26b705422b71fe3991680ce4810bc
Josh Arntz <jarntz12@gmail.com>
2017-03-27 14:33:40 -0500

    Added second view controller - in progress

--- Emoji Dictionary/ViewController.swift
+++ Emoji Dictionary/ViewController.swift
@@ -9,8 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-
+    
     @IBOutlet weak var tableview: UITableView!
+    
+    var emojis = ["😃","😎","😡","😈","💩","🖖", "👄"]
     
     
     override func viewDidLoad() {
@@ -20,16 +22,25 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         tableview.dataSource = self
         tableview.delegate = self
         
-        
-        
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return emojis.count
         
     }
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        print(indexPath.row)
+        let cell = UITableViewCell()
+        cell.textLabel?.text = emojis[indexPath.row]
+        return cell
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
